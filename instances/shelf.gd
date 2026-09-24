f9b8dfbfcf42c9c33edc9b54f119a4cd49e23d65
Dario De Vita <devitadario@gmail.com>
2026-09-25 01:52:12 +0200

    :bug: fix forklift material import

--- instances/shelf.gd
+++ instances/shelf.gd
@@ -10,8 +10,6 @@ var _btn_generate: Callable = _generate_in_engine
 @export var horiz_spots: Array[int] = [3]
 
 
-const POSTS_MESH_INSTANCE = preload("res://blend/caged_hanging_light_1k.glb")
-
 
 func _generate_in_engine() -> void:
 	pass
